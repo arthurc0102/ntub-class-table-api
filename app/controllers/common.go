56a37538ab9b4d88f5359ee9fe40333a29d57c75
Arthur Chang <arthurc0102@gmail.com>
2018-09-10 10:16:50 +0800

    Add docs link to root

--- app/controllers/common.go
+++ app/controllers/common.go
@@ -6,17 +6,17 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+const docsLink = "https://hackmd.io/_j-QIksAS46DBwtwCZ3vuw?view"
+
 // Root controller
 func Root(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "NTUB Class Table API",
+		"docs":    docsLink,
 	})
 }
 
 // Docs controller
 func Docs(c *gin.Context) {
-	c.Redirect(
-		http.StatusMovedPermanently,
-		"https://hackmd.io/_j-QIksAS46DBwtwCZ3vuw?view",
-	)
+	c.Redirect(http.StatusMovedPermanently, docsLink)
 }

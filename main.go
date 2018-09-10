a1dbd81b384f492b95c1902cca2945b2088b4677
Arthur Chang <arthurc0102@gmail.com>
2018-09-10 10:23:33 +0800

    Add cors settings

--- main.go
+++ main.go
@@ -2,15 +2,22 @@ package main
 
 import (
 	"github.com/arthurc0102/ntub-class-table-api/app/controllers"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	// Init server
 	server := gin.Default()
 
+	// Middleware
+	server.Use(cors.Default())
+
+	// Routing
 	server.GET("", controllers.Root)
 	server.GET("/docs", controllers.Docs)
 	server.GET("/personal/:id", controllers.PersonalClassTable)
 
+	// Start server
 	server.Run()
 }

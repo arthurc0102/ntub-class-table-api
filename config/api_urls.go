471df8a18426925eb443a400b1095a9ab322d9f3
Arthur Chang <arthurc0102@gmail.com>
2023-04-18 08:35:19 +0800

    Remove url trailing slash

--- config/api_urls.go
+++ config/api_urls.go
@@ -2,6 +2,6 @@ package config
 
 // API urls
 const (
-	Host          = "https://ntcbadm.ntub.edu.tw/"
+	Host          = "https://ntcbadm.ntub.edu.tw"
 	ClassTableURL = Host + "/JMobile_STD/AjaxPage/SRHCUR_Schedule_ajax.aspx"
 )
